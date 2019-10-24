e133e4e62c415a6a51b07dc4ea85ed0c4c5e2eb3
Enzo Lizama <lizama.enzo@gmail.com>
2019-10-24 02:51:47 -0500

    Add travis configuration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutter_planets/screens/HomePage.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    // await tester.pumpWidget(HomePage());
+    await tester.pumpWidget(HomePage());
 
     // Verify that our counter starts at 0.
     // expect(find.text('0'), findsOneWidget);
